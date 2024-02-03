3aa31bc8392fd98f367a1374318d3d2f33ee8dad
Olle Welin <olle_welin@hotmail.com>
2024-02-03 10:09:51 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -248,7 +248,7 @@ int main()
     next_scene_fc_net.learning_rate = 0.001;
     next_scene_fc_net.momentum = 0.1; //
     policy_fc_net.learning_rate = 0.005;
-    policy_fc_net.momentum = 0.95; //
+    policy_fc_net.momentum = 0.9; //
 
     double init_random_weight_propotion = 0.25;
     const double warm_up_epsilon_default = 0.95;
